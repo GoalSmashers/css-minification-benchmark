GoalSmashers/css-minification-benchmark

commit 27eccf22063a3131228bdab9f04e340c80ab13f0

HTML: Add more info.
* add a header
* add a link to the GitHub repo
* show the full date of the report at the bottom of the page

lib/process-output.js
@@ -10,14 +10,20 @@ const lineBreak = os.EOL;
 const write = (processedFiles, activeMinifiers, asHTML, measureGzip, showTotal, filter) => {
   process.stdout.write(lineBreak);
 
-  const minifierNames = activeMinifiers.slice(0).map(minifier => {
+  const data = activeMinifiers.slice(0).map(minifier => {
     if (asHTML && minifier.url)
       return `<a href="${minifier.url}">${minifier.name} - ${minifier.version}</a>`;
     return `${minifier.name} - ${minifier.version}`;
   });
-  minifierNames.unshift(`Generated on ${new Date().toISOString().replace(/T.+$/g, '')}`);
 
-  const opts = { head: minifierNames };
+  // this is needed for the table's first `th`
+  data.unshift('File');
+  const date = new Date();
+  data.date = {};
+  data.date.human = `${date.toUTCString()}`;
+  data.date.iso = `${date.toISOString()}`;
+
+  const opts = { data };
   const results = asHTML ? HTMLTable(opts) : new Table(opts);
   let totalLength = 0;
 
@@ -39,9 +45,9 @@ const write = (processedFiles, activeMinifiers, asHTML, measureGzip, showTotal, 
   });
 
   if (showTotal) {
-    const totalRow = [`TOTAL - ${asHTML ?
-      `<em>${totalLength} bytes</em>` :
-      `${totalLength} bytes`}`];
+    const totalRow = [`${asHTML ?
+      `<span class="text-uppercase font-weight-bold">Total</span> - <em>${totalLength} bytes</em>` :
+      `TOTAL - ${totalLength} bytes`}`];
     activeMinifiers.forEach(minifier => {
       totalRow.push(formatRow(asHTML, minifier.total, measureGzip, filter));
     });

lib/html-table.js
@@ -1,9 +1,7 @@
 const os = require('os');
 
-module.exports = options => {
-  const { head } = options;
+module.exports = ({ data }) => {
   const rows = [];
-
   const lineBreak = os.EOL;
 
   const tableRow = (columns, tag) => {
@@ -34,19 +32,26 @@ module.exports = options => {
     page.push('<meta name="viewport" content="width=device-width, initial-scale=1">');
     page.push('<title>css-minification-benchmark results</title>');
     page.push('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">');
-    page.push('<style>body{font-size:.9rem}th{text-align:center;vertical-align:middle!important}</style>');
+    page.push('<style>body{font-size:.9rem}.table thead th{text-align:center;vertical-align:middle}.table td{vertical-align:middle}</style>');
     page.push('</head>');
     page.push('<body>');
+    page.push('<div class="container-fluid">');
+    page.push('<div class="mt-2 mb-4 text-center">');
+    page.push('<h1 class="h3">CSS Minification Benchmark</h1>');
+    page.push('<a href="https://github.com/GoalSmashers/css-minification-benchmark" title="View the GitHub repository">GitHub</a>');
+    page.push('</div>');
     page.push('<div class="table-responsive">');
     page.push('<table class="table table-bordered table-sm table-hover table-striped">');
-    page.push(`<thead>${tableRow(head, '<th>')}</thead>`);
+    page.push(`<thead>${tableRow(data, '<th>')}</thead>`);
     page.push('<tbody>');
     rows.forEach(row => {
       page.push(tableRow(row, '<td>'));
     });
     page.push('</tbody>');
     page.push('</table>');
-    page.push('</div>');
+    page.push('</div>'); // table-responsive
+    page.push(`<footer class="text-muted pt-2 pb-3">Page generated on <time datetime="${data.date.iso}">${data.date.human}</time></footer>`);
+    page.push('</div>'); // container-fluid
     page.push('</body>');
     page.push('</html>');
     return page.join(lineBreak);

commit dbf5bcbdc73f18092411863d34f4d950c5afe922

Fix CLI table header.
Broken in 27eccf2.

Fixes #73.

lib/process-output.js
@@ -15,15 +15,19 @@ const write = (processedFiles, activeMinifiers, asHTML, measureGzip, showTotal, 
     return `${minifier.name} - ${minifier.version}`;
   });
 
-  // this is needed for the table's first `th`
-  data.unshift('File');
   const date = new Date();
   data.date = {};
   data.date.human = `${date.toUTCString()}`;
   data.date.iso = `${date.toISOString()}`;
 
-  const opts = { data };
-  const results = asHTML ? HTMLTable(opts) : new Table(opts);
+  // this is needed for the table's first `th`
+  if (asHTML) {
+    data.unshift('File');
+  } else {
+    data.unshift(`Generated on ${data.date.iso.replace(/T.+$/g, '')}`);
+  }
+
+  const results = asHTML ? HTMLTable({ data }) : new Table({ head: data });
   let totalLength = 0;
 
   Object.keys(processedFiles).forEach(filename => {